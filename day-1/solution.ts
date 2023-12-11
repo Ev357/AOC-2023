f835bb120f2ad0d9914a47676e5eb9f9bed1f3a6
Ev357 <evest.dev@gmail.com>
2023-12-11 23:09:48 +0100

    found 1 solution

--- day-1/solution.ts
+++ day-1/solution.ts
@@ -1,7 +1,20 @@
 const file = Bun.file("input.txt");
+// const file = Bun.file("small-input.txt");
+const input = await file.text();
 
-const text = await file.text();
+const lines = input.split("\n").filter(Boolean);
 
-console.log(text);
+const sum = lines.reduce((pv, cv) => {
+  const numbers = cv.replace(/\D/g, "");
+
+  const firstNumber = numbers[0];
+  const lastNumber = numbers[numbers.length - 1];
+
+  const calValue = parseInt(`${firstNumber}${lastNumber}`);
+
+  return pv + calValue;
+}, 0);
+
+console.log(sum);
 
 export {};
