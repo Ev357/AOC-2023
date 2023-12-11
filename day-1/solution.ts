ea0eca9402322e0fc97bb0a6a5074d060653099a
Ev357 <evest.dev@gmail.com>
2023-12-11 23:37:52 +0100

    found solution for part 2

--- day-1/solution.ts
+++ day-1/solution.ts
@@ -2,13 +2,35 @@ const file = Bun.file("input.txt");
 // const file = Bun.file("small-input.txt");
 const input = await file.text();
 
+const regex = /one|two|three|four|five|six|seven|eight|nine|[1-9]/g;
+
+const numbers = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+} as const;
+
 const lines = input.split("\n").filter(Boolean);
 
 const sum = lines.reduce((pv, cv) => {
-  const numbers = cv.replace(/\D/g, "");
+  const matches = cv.match(regex);
 
-  const firstNumber = numbers[0];
-  const lastNumber = numbers[numbers.length - 1];
+  const numberArray = matches.map((match) => {
+    if (match.length !== 1) {
+      return numbers[match];
+    }
+
+    return match;
+  });
+
+  const firstNumber = numberArray[0];
+  const lastNumber = numberArray[numberArray.length - 1];
 
   const calValue = parseInt(`${firstNumber}${lastNumber}`);
 
