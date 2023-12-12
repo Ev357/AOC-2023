ad74b7ea6612d5adee885fdbdbd5a8093870ca12
Ev357 <evest.dev@gmail.com>
2023-12-12 19:31:13 +0100

    found day-4 part-2

--- day-4/solution.ts
+++ day-4/solution.ts
@@ -36,7 +36,7 @@ const scratchcards = lines.reduce<Scratchcard[]>(
 
 const getPoints = (winCount: number) => Math.trunc(Math.pow(2, winCount - 1));
 
-const points = scratchcards.reduce((sum, scratchcard) => {
+const partOne = scratchcards.reduce((sum, scratchcard) => {
   const winCount = scratchcard.winNumbers.reduce((winCount, winNumber) => {
     if (scratchcard.myNumbers.includes(winNumber)) {
       return winCount + 1;
@@ -48,6 +48,31 @@ const points = scratchcards.reduce((sum, scratchcard) => {
   return sum + getPoints(winCount);
 }, 0);
 
-console.log(points);
+const scratchcardsV2 = scratchcards.map((scratchcard) => {
+  const winCount = scratchcard.winNumbers.reduce((winCount, winNumber) => {
+    if (scratchcard.myNumbers.includes(winNumber)) {
+      return winCount + 1;
+    }
+
+    return winCount;
+  }, 0);
+
+  const winCards = [...Array(winCount).keys()].map(
+    (card) => card + scratchcard.card + 1
+  );
+
+  return { ...scratchcard, winCount, copies: 1, winCards };
+});
+
+scratchcardsV2.map((scratchcard) => {
+  scratchcard.winCards.forEach((card) => {
+    scratchcardsV2[card - 1].copies += scratchcard.copies;
+  });
+});
+
+const partTwo = () =>
+  scratchcardsV2.reduce((sum, scratchcard) => sum + scratchcard.copies, 0);
+
+console.log(partTwo());
 
 export {};
