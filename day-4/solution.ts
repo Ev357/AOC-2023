b023a2e485b47bcb6ec665b10b219010a58d3753
Ev357 <evest.dev@gmail.com>
2023-12-12 18:27:56 +0100

    found day-4 part-1

--- day-4/solution.ts
+++ day-4/solution.ts
@@ -4,4 +4,50 @@ const input = await file.text();
 
 const lines = input.trim().split("\n");
 
+interface Scratchcard {
+  card: number;
+  winNumbers: number[];
+  myNumbers: number[];
+}
+
+const getNumbers = (string: string) =>
+  string
+    .trim()
+    .split(" ")
+    .filter(Boolean)
+    .map((number) => +number);
+
+const scratchcards = lines.reduce<Scratchcard[]>(
+  (scratchcards, line, index) => {
+    const firstSplit = line.split(":")[1].split("|");
+
+    const scratchcard = {
+      card: index + 1,
+      winNumbers: getNumbers(firstSplit[0]),
+      myNumbers: getNumbers(firstSplit[1]),
+    };
+
+    scratchcards.push(scratchcard);
+
+    return scratchcards;
+  },
+  []
+);
+
+const getPoints = (winCount: number) => Math.trunc(Math.pow(2, winCount - 1));
+
+const points = scratchcards.reduce((sum, scratchcard) => {
+  const winCount = scratchcard.winNumbers.reduce((winCount, winNumber) => {
+    if (scratchcard.myNumbers.includes(winNumber)) {
+      return winCount + 1;
+    }
+
+    return winCount;
+  }, 0);
+
+  return sum + getPoints(winCount);
+}, 0);
+
+console.log(points);
+
 export {};
